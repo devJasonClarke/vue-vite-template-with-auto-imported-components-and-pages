5cee4f0dcef00d6193ba204b489897438e8fe4a2
devJasonClarke <jettison200@gmail.com>
2022-01-06 00:16:04 -0500

    ⚡ Fixed auto importing bug on windows ⚡

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import { resolve } from 'path'
+import { resolve } from "path";
 import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 import Pages from "vite-plugin-pages";
@@ -9,45 +9,10 @@ import path from "path";
 export default defineConfig({
   plugins: [
     vue(),
-    Pages({
-      dirs: [
-        // issue #68
-        { dir: resolve(__dirname, "./src/pages"), baseRoute: "" },
-      ],
-    }),
+    Pages(),
     Components({
-      dirs: ["src/components"],
-
-      // valid file extensions for components.
-      extensions: ["vue"],
-      // search for subdirectories
-      deep: true,
-      // resolvers for custom components
-      resolvers: [],
-
-      // generate `components.d.ts` global declarations,
-      // also accepts a path for custom filename
-      dts: false,
-
-      // Allow subdirectories as namespace prefix for components.
-      directoryAsNamespace: false,
-      // Subdirectory paths for ignoring namespace prefixes
-      // works when `directoryAsNamespace: true`
-      globalNamespaces: [],
-
-      // auto import for directives
-      // default: `true` for Vue 3, `false` for Vue 2
-      // Babel is needed to do the transformation for Vue 2, it's disabled by default for performance concerns.
-      // To install Babel, run: `npm install -D @babel/parser @babel/traverse`
-      directives: true,
-
-      // filters for transforming targets
-      include: [/\.vue$/, /\.vue\?vue/],
-      exclude: [
-        /[\\/]node_modules[\\/]/,
-        /[\\/]\.git[\\/]/,
-        /[\\/]\.nuxt[\\/]/,
-      ],
+      importPathTransform: (path) =>
+        path.startsWith("C:") ? path.replaceAll("\\", "\\\\") : path,
     }),
   ],
 
